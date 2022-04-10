4929a85d71a274025ed3ebd9b655c99016c9fce9
david <davidomej@hotmail.com>
2022-04-10 17:58:16 +0100

    After Refactor

--- src/main/kotlin/CsvFilter.kt
+++ src/main/kotlin/CsvFilter.kt
@@ -4,10 +4,16 @@ class CsvFilter {
      result.add(lines[0])
      val invoice = lines[1]
      val fields = invoice.split(',')
-     if((fields[4].isNullOrEmpty() || fields[5].isNullOrEmpty()) &&
-             (!(fields[4].isNullOrEmpty() && fields[5].isNullOrEmpty()))){
+     val ivaFieldIndex = 4
+     val igicFieldIndex  =5
+     val taxFieldsAreMutuallyExclusive=(fields[ivaFieldIndex].isNullOrEmpty() ||
+             fields[igicFieldIndex].isNullOrEmpty()) &&
+             (!(fields[ivaFieldIndex].isNullOrEmpty() &&
+                     fields[igicFieldIndex].isNullOrEmpty()))
+         if(taxFieldsAreMutuallyExclusive){
+
          result.add(lines[1])
-     }
+         }
      return result.toList()
  }
 }
