7c84a599056001d322b9c09ff8abe7b282c1b5c1
david <davidomej@hotmail.com>
2022-04-10 17:45:43 +0100

    Green Test exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive

--- src/test/kotlin/CsvFilterShould.kt
+++ src/test/kotlin/CsvFilterShould.kt
@@ -6,10 +6,19 @@ class CsvFilterShould {
     @Test
     fun allow_for_correct_lines_only(){
         val headerLine="Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, Cif_cliente, Nif_cliente"
-        val invoiceLine="1,02/05/2019, 1000, 810, 19,, ACER Laptop,B76430134,"
+        val invoiceLine="1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
 
         val result=CsvFilter().filter(listOf(headerLine, invoiceLine))
 
         assertThat(result).isEqualTo(listOf(headerLine, invoiceLine))
+    }
+
+    @Test
+    fun exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive(){
+        val headerLine="Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        val invoiceLine="1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
+        val result=CsvFilter().filter(listOf(headerLine,invoiceLine))
+
+        assertThat(result).isEqualTo(listOf(headerLine))
     }
 }
